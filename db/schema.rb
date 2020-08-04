95a6dd93c012c226044339b8f4a4aa0c47a0a8b7
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-04 09:54:44 -0600

    Patient model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200804154709) do
+ActiveRecord::Schema.define(version: 20200804155234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20200804154709) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "doctor_id"
+    t.index ["doctor_id"], name: "index_patients_on_doctor_id"
   end
 
   add_foreign_key "doctors", "hospitals"
+  add_foreign_key "patients", "doctors"
 end
