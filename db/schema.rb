5bdb1f9f9cc6fb2c926a1f8726117c849c01118c
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-04 09:42:08 -0600

    Hospital and doctor migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200804151606) do
+ActiveRecord::Schema.define(version: 20200804153103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20200804151606) do
     t.string "education"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "hospital_id"
+    t.index ["hospital_id"], name: "index_doctors_on_hospital_id"
   end
 
+  create_table "hospitals", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "doctors", "hospitals"
 end
