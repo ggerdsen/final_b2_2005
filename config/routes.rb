935e1918f4a820258d1295fe2806b212aef01f71
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-04 11:31:02 -0600

    Remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/doctors/:doctor_id", to: "doctors#show"
   get "/hospitals/:hospital_id", to: "hospitals#show"
   delete "/doctors/:patient_id", to: "doctors#destroy"
