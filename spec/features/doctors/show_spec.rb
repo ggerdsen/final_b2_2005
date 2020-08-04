61a22cbf5bb778f522267ca3175b9383a8182dea
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-04 09:43:31 -0600

    Update test to include hospitals and patients

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Doctors Show Page' do
   before :each do
-    @grey_doctor = Doctor.create(name: "Meredith Grey", specialty: "general surgery", education: "Harvard University")
+    @sloan_hostpital = Hospital.create(name: "Grey Sloan Memorial Hospital")
+    @grey_doctor = Doctor.create(name: "Meredith Grey", specialty: "general surgery", education: "Harvard University", hospital_id: @sloan_hostpital.id)
   end
   
   it "As a visitor I see all doctor information" do
 
     visit "/doctors/#{@grey_doctor.id}"
     
-    expect(page).to have_content(@grey_doctor.name)
-    expect(page).to have_content(@grey_doctor.specialty)
-    expect(page).to have_content(@grey_doctor.education)
-
+    expect(page).to have_content("Doctor: #{@grey_doctor.name}")
+    expect(page).to have_content("Specialty: #{@grey_doctor.specialty}")
+    expect(page).to have_content("Education: #{@grey_doctor.education}")
+    expect(page).to have_content("Hospital: #{@grey_doctor.hospital.name}")
   end
 end
   # As a visitor
